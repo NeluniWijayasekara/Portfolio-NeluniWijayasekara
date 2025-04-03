b66811a9a6ea4d1a0955aa5099ac02e20a541d4c
Nelu <neluni09@gmail.com>
2025-04-04 02:59:38 +0530

    make responsive

--- app/components/ToolList.jsx
+++ app/components/ToolList.jsx
@@ -9,7 +9,7 @@ const ToolList = ({ tools }) => {
   return (
     <div className='relative flex items-center justify-center'>
       {/* First group of tools */}
-      <div className='absolute right-[-60px]  top-1/2 transform-translate-y-1/2  p-4 flex flex-col gap-4 opacity-0 group-hover:opacity-100 transition-all duration-300 transform group-hover:translate-x-0 translate-x-4 bg-white bg-opacity-90 rounded-lg shadow-lg hover:shadow-2xl border border-gray-200'>
+      <div className='absolute right-[-60px] sm:right-[-80px] top-1/2 transform -translate-y-1/2 p-4 flex flex-col gap-4 opacity-0 group-hover:opacity-100 transition-all duration-300 transform group-hover:translate-x-0 translate-x-4 bg-white bg-opacity-90 rounded-lg shadow-lg hover:shadow-2xl border border-gray-200'>
         {firstHalf.map((tool, i) => (
           <div key={i} className='w-12 h-12 flex items-center justify-center bg-gray-100 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300'>
             <Image 
@@ -25,7 +25,7 @@ const ToolList = ({ tools }) => {
 
       {/* Second group of tools (if more than 4) */}
       {secondHalf.length > 0 && (
-        <div className='absolute left-[-60px] top-1/2 transform-translate-y-1/2 p-4 flex flex-col gap-4 opacity-0 group-hover:opacity-100 transition-all duration-300 transform group-hover:translate-x-0 translate-x-5 bg-white bg-opacity-90 rounded-lg shadow-lg hover:shadow-2xl border border-gray-200'>
+        <div className='absolute left-[-60px] sm:left-[-80px] top-1/2 transform -translate-y-1/2 p-4 flex flex-col gap-4 opacity-0 group-hover:opacity-100 transition-all duration-300 transform group-hover:translate-x-0 translate-x-5 bg-white bg-opacity-90 rounded-lg shadow-lg hover:shadow-2xl border border-gray-200'>
           {secondHalf.map((tool, i) => (
             <div key={i} className='w-12 h-12 flex items-center justify-center bg-gray-100 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300'>
               <Image 
--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -7,9 +7,9 @@ import ToolList from './ToolList'; // Import the ToolList component
 
 const Work = () => {
   return (
-    <div id='work' className='w-full px-[10%] py-12 scroll-mt-20'>
+    <div id='work' className='w-full px-4 sm:px-10 py-12 scroll-mt-20'>
        <h4 className='text-center mb-2 text-lg  text-gray-700'>My portfolio</h4>
-       <h2 className='text-center text-5xl font-Ovo'>My latest work</h2>
+       <h2 className='text-center text-3xl sm:text-5xl font-Ovo'>My latest work</h2>
 
        <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
         Welcome to my web development portfolio! Explore a collection of 
